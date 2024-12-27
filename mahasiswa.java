531097184337b50842b26ba4e1c3d992a32554a8
icedd777 <dumatubunrisat@gmail.com>
2024-12-27 17:12:00 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class mahasiswa {
+     void menulis(){
+        System.out.println("MAHASISWA SEDANG Menulis KATA GOMBALAN");
+    }
+    void nyontek(){
+        System.out.println("MAHASISWA SEDANG NYONTEK KETIKA UAS");
+    }
+    void modifikasi(){
+        System.out.println("MAHASISWA SEDANG MEMODIFIKIKASI PC");
+    }
+    
+}
+    
+
---
+++ mahasiswaBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class mahasiswaBeraksi {
+     public static void main(String[] args) {
+    mahasiswa ms = new mahasiswa();
+    ms.menulis();
+    ms.nyontek();
+    ms.modifikasi();
+    
+    }
+    
+}
+    
+
